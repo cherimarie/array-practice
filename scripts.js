3021e3273d6e549d7c85b6f240e01be937d96672
Cheri Allen <cherimarie@gmail.com>
2017-02-25 15:31:01 -0800

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,9 @@
+alert("Let's get to work! ")
+// TODO: Build an array of strings that could be answers to magic 8 ball type questions.
+
+
+/* TODO: Write a form handler function that will:
+  1. capture the question asked in the form
+  2. randomly select an element from the array of answers
+  3. console log something conversational to user, like "Oh, you want to know [question]? Well, I think... [answer]"
+*/
