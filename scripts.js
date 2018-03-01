ea4b69252f53cb993dccbd0e08e08100da3938f8
Cheri Allen <cherimarie@gmail.com>
2018-02-28 20:36:03 -0800

    Clarify TODOs

--- scripts.js
+++ scripts.js
@@ -1,9 +1,11 @@
 alert("Let's get to work! ")
-// TODO: Build an array of strings that could be answers to magic 8 ball type questions.
 
+// TODO: Assign value of "answers" variable to an array of strings
+// that could be answers to magic 8 ball type questions
+var answers
 
 /* TODO: Write a form handler function that will:
-  1. capture the question asked in the form
-  2. randomly select an element from the array of answers
-  3. console log something conversational to user, like "Oh, you want to know [question]? Well, I think... [answer]"
+  1. capture the question asked in the form, and assign it to a variable
+  2. console log something conversational including the question, like
+     "Oh, you want to know if you'll win the lottery?"
 */
